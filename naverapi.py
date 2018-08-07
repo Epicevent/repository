6e0e2d5358769b9735e86d6fa54f218f19ced17a
Epicevent <rocky0516@naver.com>
2018-08-07 11:33:13 +0900

    GUI test

--- naverapi.py
+++ naverapi.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import urllib.request
+import requests
 import json    # or `import simplejson as json` if on Python < 2.6
+import io
 
-def staticmap(center,level=12,w=640,h=640,baselayer='default'):
+def staticmap(center,level=12,w=640,h=640,baselayer='default',crs='EPSG:4258'):
+
     '''
     :param center:(XYstring)이미지의 중심 좌표(X좌표, Y좌표)이다. 이 중심 좌표를 기준으로 요청한 w, h 픽셀 크기로 이미지를 생성한다.
     :param level: (integer) 	네이버지도 서비스에 정의되어 있는 줌 레벨이다. 최소1레벨 최대14레벨까지 지원한다.
@@ -12,6 +15,16 @@ def staticmap(center,level=12,w=640,h=640,baselayer='default'):
                        'default' : 일반 지도
                        'satellite' : 위성 지도
                        'bl_vc_bg' : 주기가 없는 배경 지도
+    :param crs :
+                     1. EPSG:4326 : WGS84 경위도
+                     2. NHN:2048 : UTMK
+                     3. NHN:128 : 카텍 TM128
+                     4. EPSG:4258 : GRS80 경위도
+                     5. EPSG:4162 : Bessel 경위도
+                     6. EPSG:2096 : Korea East Belt
+                     7. EPSG:2097 : Korea Central Belt
+                     8. EPSG:2098 : Korea West Belt
+                     9. EPSG:3857 or EPSG:900913 : Google Maps
     :return:요청 변수들에 맞게 한 장의 이미지 파일(png나 jpeg)을 반환합니다.
     '''
 
@@ -20,12 +33,20 @@ def staticmap(center,level=12,w=640,h=640,baselayer='default'):
     url = str('https://openapi.naver.com/v1/map/staticmap.bin?clientId='+ client_id
               +'&url='+ myURL
               + '&center='+center
-              + '&level=14&w='+str(w)+'&h='+str(h)
-              +  '&baselayer='+baselayer)
+              + '&level='+str(level)
+              + '&w='+str(w)+'&h='+str(h)
+              +  '&baselayer='+baselayer
+              +  '&crs=' +crs
+             )
+    try:
+        response = requests.get(url=url)
+    except:
+        print('api요청이 응답하지 않습니다.')
+    else:
+        image_data = response.content
+        image = Image.open(io.BytesIO(image_data))
+    return image
 
-    imageByte =  urllib.request.urlopen(url).read()
-
-    return imageByte
 
 def geocode(addressString):
     obj = dict()
@@ -113,5 +134,5 @@ def XYtoAddrdetailList( XYstring ):
 if __name__ =="__main__":
     print (XYtoAddrdetailList("126.8397859,37.4991205"))
     print (geocode("서울특별시 구로구 오류동 338"))
-    data=staticmap("126.8397859,37.4991205")
-    print(data)
+    staticmap("126.8397859,37.4991205")
+
--- testGUI.py
+++ testGUI.py
@@ -1,33 +1,11 @@
-import tkinter as tk
-from tkinter import filedialog
-
-# this class is an instance of a Frame. It is not required to do it this way.
-# this is just my preferred method.
-class ReadFile(tk.Frame):
-    def __init__(self):
-        tk.Frame.__init__(self)
-        # we need to make sure that this instance of tk.Frame is visible.
-        self.pack()
-        # create Button that link to methods used to get file path.
-        tk.Button(self, text="Open file", command=self.open_and_prep).pack()
-        # create Button that link to methods used to process said file.
-        tk.Button(self, text="Print Content", command=self.process_open_file).pack()
-
-    def open_and_prep(self):
-        # askopefilename is used to retrieve the file path and file name.
-        self.file_path = filedialog.askopenfilename()
-
-    def process_open_file(self):
-        # do what you want with the file here.
-        if self.file_path != "":
-            # opens file from file path and prints each line.
-            with open(self.file_path,"r") as testr:
-                for line in testr:
-                    print (line)
-
-if __name__ == "__main__":
-    # tkinter requires one use of Tk() to start GUI
-    root = tk.Tk()
-    TestApp = ReadFile()
-    # tkinter requires one use of mainloop() to manage the loop and updates of the GUI
-    root.mainloop()
+import tkinter
+import naverapi
+from PIL import ImageTk
+root = tkinter.Tk()
+root.geometry('1000x1000')
+canvas = tkinter.Canvas(root,width=999,height=999)
+canvas.pack()
+pilImage = naverapi.staticmap("126.8397859,37.4991205",level=14)
+image = ImageTk.PhotoImage(pilImage)
+imagesprite = canvas.create_image(400,400,image=image)
+root.mainloop()
